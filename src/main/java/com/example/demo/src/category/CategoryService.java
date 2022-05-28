8fbd710b36b60d4af3d7d180c92d22714df79b00
somyeong <thaud8487@naver.com>
2022-05-28 17:34:21 +0900

    chore: cross origin 삭제

--- src/main/java/com/example/demo/src/category/CategoryService.java
+++ src/main/java/com/example/demo/src/category/CategoryService.java
@@ -2,6 +2,9 @@ package com.example.demo.src.category;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.src.category.model.CategorySimple;
+import com.example.demo.src.category.model.Req.PostSearchReq;
+import com.example.demo.src.category.model.Res.PostSearchRes;
+import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +20,29 @@ public class CategoryService {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final CategoryDao categoryDao;
+    private final JwtService jwtService;
 
 
     @Autowired
-    public CategoryService(CategoryDao categoryDao){
+    public CategoryService(CategoryDao categoryDao, JwtService jwtService){
         this.categoryDao=categoryDao;
+        this.jwtService=jwtService;
     }
 
     public List<CategorySimple> getCategoryList() throws BaseException{
         try{
             List<CategorySimple> categorySimpleList = categoryDao.getCategoryList();
             return categorySimpleList;
+        }
+        catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public PostSearchRes createSearch(int userIdx, PostSearchReq postSearchReq) throws BaseException {
+        try{
+            int search_id = categoryDao.createSearch(userIdx, postSearchReq.getCategory_name());
+            return new PostSearchRes(search_id);
         }
         catch(Exception exception){
             throw new BaseException(DATABASE_ERROR);
---
+++ src/main/java/com/example/demo/src/category/model/Req/PostSearchReq.java
@@ -0,0 +1,12 @@
+package com.example.demo.src.category.model.Req;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostSearchReq {
+    private String category_name;
+}
