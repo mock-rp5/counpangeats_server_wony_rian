121317f71eeee32cfbdc2d8470fd35a024f8f5bc
somyeong <thaud8487@naver.com>
2022-06-01 13:48:15 +0900

    fix: 코드정리

--- src/main/java/com/example/demo/src/kakao/KakaoController.java
+++ src/main/java/com/example/demo/src/kakao/KakaoController.java
@@ -3,9 +3,7 @@ package com.example.demo.src.kakao;
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.user.UserProvider;
-import com.example.demo.src.user.UserService;
 import com.example.demo.src.user.model.Res.PostLoginRes;
-import com.example.demo.utils.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,23 +21,17 @@ public class KakaoController {
     private KakaoAPI kakao;
     @Autowired
     private final UserProvider userProvider;
-    @Autowired
-    private final UserService userService;
-    @Autowired
-    private final JwtService jwtService;
 
-    public KakaoController(KakaoAPI kakao, UserProvider userProvider, UserService userService, JwtService jwtService) {
+    public KakaoController(KakaoAPI kakao, UserProvider userProvider) {
         this.kakao = kakao;
         this.userProvider = userProvider;
-        this.userService = userService;
-        this.jwtService = jwtService;
     }
 
-//    @PostMapping("/login/access-token")
-//    public void getAccessToken(@RequestParam("code") String code) {
-//        String access_Token = kakao.getAccessToken(code);
-//        System.out.println("controller access_token : " + access_Token);
-//    }
+    @PostMapping("/login/access-token")
+    public void getAccessToken(@RequestParam("code") String code) {
+        String access_Token = kakao.getAccessToken(code);
+        System.out.println("controller access_token : " + access_Token);
+    }
 
 
     @PostMapping("/login/kakao")
@@ -61,30 +53,5 @@ public class KakaoController {
         }
 
     }
-
-//    /**
-//     * 카카오 로그인 API
-//     * [POST] /users/login/kakao
-//     * @return BaseResponse<PostLoginRes>
-//     */
-//    @PostMapping("/login/kakao")
-//    public BaseResponse<PostLoginRes> kakaoLogin(@RequestParam("code") String code) throws BaseException {
-//
-//        KakaoUserInfo userInfo = kakaoService.signIn(code);
-//        if (userInfo.getEmail()==null || userProvider.checkUserByEmail(userInfo.getEmail()) == 0){
-//            return new BaseResponse<>(EMPTY_USER_EMAIL);
-//        }
-//
-//        User user = userProvider.getUserInfoKakao(userInfo.getEmail());
-//
-//        int userIdx = user.getUserIdx();
-//        String jwt = jwtService.createJwt(userIdx);
-//
-//        UserNowAddressInfo userNowAddressInfo = userProvider.getUserNowInfo(userIdx);
-//
-//        return new BaseResponse<>(new PostKakaoSignInRes(userIdx,jwt, userNowAddressInfo));
-//
-//    }
-
 
 }
